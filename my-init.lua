027662126c2ced13c9a23ac917ccdc237808724d
rufus <rufus.oelkers@arbourglobal.com>
2021-08-26 15:22:59 +0200

    update config for august

--- my-init.lua
+++ my-init.lua
@@ -1,6 +1,5 @@
 --
 --LSP colors
---(reset after colorscheme has been applied)
 --
 
 vim.cmd([[
@@ -102,6 +101,7 @@ key_mapper('', '<leader>gc', ':lua require"telescope.builtin".git_commits()<CR>'
 key_mapper('', '<leader>gb', ':lua require"telescope.builtin".git_branches()<CR>')
 key_mapper('', '<leader>gs', ':lua require"telescope.builtin".git_status()<CR>')
 key_mapper('', '<leader>o', ':lua require"telescope.builtin".oldfiles()<CR>')
+key_mapper('', '<leader>c', ':cclose<CR>')
 key_mapper('n', 's', ':HopWord<CR>')
 key_mapper('n', '<leader>dt', ':Gitsigns diffthis<CR>')
 key_mapper('n', '<leader>dv', ':DiffviewOpen<CR>')
@@ -174,6 +174,9 @@ packer.startup(function()
 
   use 'glepnir/dashboard-nvim'
   use 'kdheepak/lazygit.nvim'
+
+  use 'jose-elias-alvarez/null-ls.nvim'
+  use 'jose-elias-alvarez/nvim-lsp-ts-utils'
  end
 )
 
@@ -268,27 +271,19 @@ require('telescope').setup({
       },
       prompt_position = "top",
     },
-    file_ignore_patterns = { "node_modules" }, 
+    file_ignore_patterns = { "node_modules", "/home/rufus/Dev/arbour/common" }, 
     sorting_strategy = "ascending"
   },
   pickers = {
-    live_grep = {
-      previewer = false
-    },
-    find_files = {
-      previewer = false,
-    },
     oldfiles = {
       include_current_session = true,
       sort_lastused = true,
       theme = "dropdown",
-      previewer = false,
       cwd_only = false 
     },
     buffers = {
       sort_lastused = true,
       theme = "dropdown",
-      previewer = false,
       mappings = {
         i = {
           ["<c-d>"] = require("telescope.actions").delete_buffer,
@@ -333,7 +328,57 @@ configs.setup {
 local lspconfig = require'lspconfig'
 local function custom_on_attach(client)
   print('Attaching to ' .. client.name)
+  client.resolved_capabilities.document_formatting = false
+  local ts_utils = require("nvim-lsp-ts-utils")
+
+  -- defaults
+  ts_utils.setup {
+      disable_commands = false,
+      enable_import_on_completion = false,
+
+      -- import all
+      import_all_timeout = 5000, -- ms
+      import_all_priorities = {
+          buffers = 4, -- loaded buffer names
+          buffer_content = 3, -- loaded buffer content
+          local_files = 2, -- git files or files with relative path markers
+          same_file = 1, -- add to existing import statement
+      },
+      import_all_scan_buffers = 100,
+      import_all_select_source = false,
+
+      -- eslint
+      eslint_enable_code_actions = true,
+      eslint_enable_disable_comments = true,
+      eslint_bin = "eslint",
+      eslint_config_fallback = nil,
+      eslint_enable_diagnostics = false,
+      eslint_show_rule_id = false,
+
+      -- formatting
+      enable_formatting = true,
+      formatter = "prettier",
+      formatter_config_fallback = nil,
+
+      -- update imports on file move
+      update_imports_on_move = false,
+      require_confirmation_on_move = false,
+      watch_dir = nil,
+  }
+
+  -- required to fix code action ranges
+  ts_utils.setup_client(client)
+
+  -- no default maps, so you may want to define some here
+  local opts = {silent = true}
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "go", ":TSLspOrganize<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "qq", ":TSLspFixCurrent<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", ":TSLspRenameFile<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", ":TSLspImportAll<CR>", opts)
+
+  --vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()")
 end
+
 local default_config = {
   on_attach = custom_on_attach,
 }
@@ -348,6 +393,28 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     update_in_insert = true,
   }
 )
+
+--
+--Formatting
+--
+require("null-ls").setup({
+    sources = {
+      require("null-ls").builtins.formatting.prettier.with {
+        filetypes = {
+          "typescriptreact",
+          "typescript",
+          "javascriptreact",
+          "javascript",
+          "svelte",
+          "json",
+          "jsonc",
+          "css",
+          "html",
+        },
+      }
+    }
+})
+require('lspconfig')["null-ls"].setup {}
 
 --
 --Galaxyline
@@ -622,3 +689,4 @@ vim.cmd[[
 vim.cmd "let g:dashboard_session_directory = $HOME..'/.config/nvim/.sessions'"
 
 vim.cmd("colorscheme aurora")
+vim.cmd("set noequalalways")
